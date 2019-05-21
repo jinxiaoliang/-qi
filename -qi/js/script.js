d6c7dfb4068fa166f8b2be14ff4232c6bacaf18b
jinxiaoliang <1848815766@qq.com>
2019-05-21 22:21:38 +0800

    chooce

---
+++ -qi/js/script.js
@@ -0,0 +1,37 @@
+var data=['干锅','串串','炒菜','冒菜','面米线','火锅','鸡公煲','肥宅套餐','食堂'],
+    timer=null,
+    flag=0;
+    var title=document.getElementById('title'),
+        play=document.getElementById('play'),
+        stop=document.getElementById('stop');
+window.onload=function(){
+  var title=document.getElementById('title'),
+      play=document.getElementById('play'),
+      stop=document.getElementById('stop');
+      play.onclick=playFun;
+      stop.onclick=stopFun;
+      document.onkeyup=function(event){
+        event=event||window.event;
+        if(event.keyCode==13){
+          if(flag==0){
+            playFun();
+            flag=1;
+          }else{
+            stopFun();
+            flag=0;
+          }
+        }
+      }
+      function playFun(){
+        clearInterval(timer);
+        timer=setInterval(function(){
+          var random=Math.floor(Math.random()*data.length);
+          title.innerHTML=data[random];
+        },50);
+        play.style.background='#999';
+      }
+      function stopFun(){
+        clearInterval(timer);
+        play.style.background='#036';
+      }
+}
